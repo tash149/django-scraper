22599c812d4f77b0c8362c51a233e34967a0d49f
tash149 <tarushisharmaace@gmail.com>
2019-09-22 17:50:02 +0530

    backend: search using quote_plus working

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from requests.compat import quote_plus
 import requests
 
-
 # Create your views here.
+
+BASE_CRAIGLIST_URL = 'https://jaipur.craigslist.org/search/jjj?query={}'
+
 
 def home(request):
     return render(request, template_name='base.html')
@@ -11,6 +14,12 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')  # as we have named the placeholder search in newsearch.html
-    print(search)
+    print(quote_plus(search))
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    print('>>>>')
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+
     stuff_for_frontend = {'search': search}
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
