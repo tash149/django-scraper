8da3b91ca8a8078918eb870c0a8665b30956df90
tash149 <tarushisharmaace@gmail.com>
2019-09-23 15:41:32 +0530

    backend: post details added

--- my_app/models.py
+++ my_app/models.py
@@ -8,9 +8,7 @@ class Search(models.Model):
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '{]'.format(self.search)         # when we don't write this admin site would have searches with search objects, writing this would allow the actual search topic on the list
-
+        return '{}'.format(self.search)  # when we don't write this admin site would have searches with search objects, writing this would allow the actual search topic on the list
 
     class Meta:
         verbose_name_plural = 'Searches'  # when we have registered our models in the admin.py we would need to change the interface from Searchs to Searches
-
--- my_app/views.py
+++ my_app/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from bs4 import BeautifulSoup
 from requests.compat import quote_plus
 import requests
+from . import models
 
 # Create your views here.
 
-BASE_CRAIGLIST_URL = 'https://jaipur.craigslist.org/search/jjj?query={}'
+BASE_CRAIGLIST_URL = 'https://jaipur.craigslist.org/search/?query={}'#'https://jaipur.craigslist.org/search/jjj?query={}'
 
 
 def home(request):
@@ -14,12 +15,28 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')  # as we have named the placeholder search in newsearch.html
+    models.Search.objects.create(search = search)
     print(quote_plus(search))
     final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
-    print('>>>>')
     print(final_url)
     response = requests.get(final_url)
     data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
 
+    # post_titles = soup.find_all('a', {'class': 'result-title'})
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    print(post_listings[0])
+
+    post_title = post_listings[0].find('a', {'class': 'result-title'}).text
+    post_url = post_listings[0].find('a').get('href')
+    post_price = post_listings[0].find('a', {'class': 'result-price'}).text
+
+    print('>>>>')
+    print(post_title)
+    print(post_url)
+    print(post_price)
+
+    # yaha pe baaki sab kuch
     stuff_for_frontend = {'search': search}
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
+    # print(post_titles[0].get('href')) # post_titles[0].text
